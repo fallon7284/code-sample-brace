b3e4b97c205db40dd9129fc18dbe79bb32d37e76
Brendan Fallon <brendanc.fallon@gmail.com>
2019-07-09 15:44:23 -0400

    initial data fetching

---
+++ src/components/BeerList.js
@@ -0,0 +1,31 @@
+import React from 'react'
+// import fetchBeers from '../functions/fetchBeers'
+
+export default class BeerList extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            beers: [],
+            selectedBeer: null,
+            page: 1,
+            perPage: 10
+        }
+    }
+
+    componentDidMount(){
+        this.setBeers()
+    }
+
+    async setBeers(){
+        let beers = await fetch(`https://api.openbrewerydb.org/breweries?page=${this.state.page}&per_page=${this.state.perPage}`)
+        beers = await beers.json()
+        this.setState({beers})
+    }
+
+    render(){
+        console.log(this.state.beers)
+        return (
+            <div>Beers go here</div>
+        )
+    }
+}
---
+++ src/components/InitialDisplay.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default () => {
+    return (
+        <div className="default-view">
+            <h1 className="header">Book Store</h1>
+            
+        </div>
+    )
+}
